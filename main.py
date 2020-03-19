b034e8d68df20d8bf5e44ed92c987bcf286540ce
Tom Naumann <tom.naumann.95@gmail.com>
2020-03-20 00:42:28 +0100

    docstring to simple markdown

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ parser.add_argument('--target', type=str, help='list of excluded files resp. fol
 parser.add_argument('--exclude', nargs='*', help='list of excluded files resp. folders', default=[])
 
 args = parser.parse_args()
-# args.module = "src/mydocstringtest/"
 
 files = [f for f in Path(args.module).rglob('*.py')
          if not f.name.startswith('__init__')]
@@ -24,60 +23,61 @@ for e in args.exclude:
     files = [f for f in files
              if not str(f).startswith(e)]
 
+
+def write(file, text):
+    file.write(text + "\n")
+
+
 for file in files:
-    path = os.path.splitext(str(file))[0] + ".md"  # change file extension from '.py' to '.md'
-    path = os.path.relpath(path, args.module)  # cut the path to module
-    path = os.path.join(args.target, path)  # join the relative path with the target folder
+    relpath = os.path.relpath(str(file), args.module)  # cut the path to module
+    target_path = os.path.join(args.target, relpath)  # join the relative path with the target folder
+    path = os.path.splitext(target_path)[0] + ".md"  # change file extension '.py' to '.md'
     os.makedirs(os.path.dirname(path))  # create directories if not existing
-    f = open(path, "x")
-    f.write("Now the file has more content!")
-    f.close()
+    with open(path, "x") as output:
+        with open(str(file)) as fd:
+            file_contents = fd.read()
+        module = ast.parse(file_contents)
 
+        write(output, "# {}".format(os.path.basename(str(file))))
+        write(output, "*{}*".format(relpath.replace("\\", "/")))
 
-    # with open(str(file)) as fd:
-    #     file_contents = fd.read()
-    #
-    # module = ast.parse(file_contents)
-    #
-    #
-    # def extract(nodes, level):
-    #     for node in nodes:
-    #         if level == 2 and isinstance(node, ast.Expr):
-    #             docstring = None
-    #             try:
-    #                 docstring = node.value.s
-    #             except AttributeError:
-    #                 pass
-    #             if docstring is not None:
-    #                 print("```")
-    #                 print(node.value.s[1:-1])
-    #                 print("```")
-    #
-    #         if isinstance(node, ast.FunctionDef) or isinstance(node, ast.ClassDef):
-    #             headline = "{} {}".format("#"*level, node.name)
-    #             if isinstance(node, ast.FunctionDef):
-    #                 if len(node.args.args) > 0:
-    #                     headline = headline + "("
-    #
-    #                 for i, par in enumerate(node.args.args):
-    #                     if i != 0:
-    #                         headline = headline + ", "
-    #                     # headline = headline + par.arg + ": " + type(par.arg).__name__
-    #                     headline = headline + par.arg
-    #
-    #                 if len(node.args.args) > 0:
-    #                     headline = headline + ")"
-    #             print(headline)
-    #
-    #             if ast.get_docstring(node) is not None:
-    #                 print("```")
-    #                 print(textwrap.dedent(ast.get_docstring(node)))
-    #                 print("```")
-    #             else:
-    #                 print("Description missing.")
-    #
-    #             if len(node.body) > 0:
-    #                 extract(node.body, level+1)
-    #
-    #
-    # extract(module.body, 2)
+        def extract(nodes, level):
+            for node in nodes:
+                if level == 2 and isinstance(node, ast.Expr):
+                    docstring = None
+                    try:
+                        docstring = node.value.s
+                    except AttributeError:
+                        pass
+                    if docstring is not None:
+                        write(output, "```")
+                        write(output, node.value.s[1:-1])
+                        write(output, "```")
+    
+                if isinstance(node, ast.FunctionDef) or isinstance(node, ast.ClassDef):
+                    headline = "{} {}".format("#"*level, node.name)
+                    if isinstance(node, ast.FunctionDef):
+                        if len(node.args.args) > 0:
+                            headline = headline + "("
+    
+                        for i, par in enumerate(node.args.args):
+                            if i != 0:
+                                headline = headline + ", "
+                            # headline = headline + par.arg + ": " + type(par.arg).__name__
+                            headline = headline + par.arg
+    
+                        if len(node.args.args) > 0:
+                            headline = headline + ")"
+                    write(output, headline)
+    
+                    if ast.get_docstring(node) is not None:
+                        write(output, "```")
+                        write(output, textwrap.dedent(ast.get_docstring(node)))
+                        write(output, "```")
+                    else:
+                        write(output, "Description missing.")
+    
+                    if len(node.body) > 0:
+                        extract(node.body, level+1)
+
+        extract(module.body, 2)
