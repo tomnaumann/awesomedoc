a4447335eb714641dfff35f57e34f74786ce2d51
Tom Naumann <tom.naumann.95@gmail.com>
2020-03-30 00:28:09 +0200

    Github link updated

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='awesomedoc',
     packages=['awesomedoc'],
-    version='0.0.2.alpha',
+    version='0.0.3.alpha',
     license='MIT',
     description='Generate simple markdown from python scripts',
     author='Tom Naumann',
     author_email='tom.naumann.95@gmail.com',
-    url="https://github.com/tomnaumann/pydoc",
+    url="https://github.com/tomnaumann/awesomedoc",
     keywords=['markdown', 'awesomedoc', 'documentation'],
     classifiers=[
         'Environment :: Console',
